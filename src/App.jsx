007b1180adae9a6b18b4d2a0669a112bd6f908b6
sik2 <bickwang123@gmail.com>
2026-03-16 11:18:52 +0900

    015

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-import { useRef, useState } from 'react'
 import TodoList from './components/TodoList'
 import TodoWriteForm from './components/TodoWriteForm'
+import { useTodos } from './hooks/useTodos'
 
 function App() {
-    const lastId = useRef(4)
-
-    const [todos, setTodos] = useState([
-        { id: 3, text: '공부하기', checked: true },
-        { id: 2, text: '야구보기', checked: false },
-        { id: 1, text: '저녁먹기', checked: true },
-    ])
-
-    const addTodo = (text) => {
-        setTodos([{ id: lastId.current, text, checked: false }, ...todos])
-        lastId.current++
-    }
-
-    const removeTodo = (seletedId) => {
-        const fileterTodos = todos.filter((todo) => todo.id != seletedId)
-        setTodos(fileterTodos)
-    }
-
-    const ToggleTodo = (seletedId) => {
-        const updateTodos = todos.map((todo) => (todo.id == seletedId ? { ...todo, checked: !todo.checked } : todo))
-        setTodos(updateTodos)
-    }
-
+    const { todos, addTodo, removeTodo, ToggleTodo } = useTodos()
     return (
         <>
             <TodoWriteForm addTodo={addTodo} />
---
+++ src/hooks/useTodos.js
@@ -0,0 +1,33 @@
+import { useRef, useState } from 'react'
+
+export function useTodos() {
+    const lastId = useRef(4)
+
+    const [todos, setTodos] = useState([
+        { id: 3, text: '공부하기', checked: true },
+        { id: 2, text: '야구보기', checked: false },
+        { id: 1, text: '저녁먹기', checked: true },
+    ])
+
+    const addTodo = (text) => {
+        setTodos([{ id: lastId.current, text, checked: false }, ...todos])
+        lastId.current++
+    }
+
+    const removeTodo = (seletedId) => {
+        const fileterTodos = todos.filter((todo) => todo.id != seletedId)
+        setTodos(fileterTodos)
+    }
+
+    const ToggleTodo = (seletedId) => {
+        const updateTodos = todos.map((todo) => (todo.id == seletedId ? { ...todo, checked: !todo.checked } : todo))
+        setTodos(updateTodos)
+    }
+
+    return {
+        todos,
+        addTodo,
+        removeTodo,
+        ToggleTodo,
+    }
+}
