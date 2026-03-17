d2689984b7b307fca8171d7cd7c1b10478868d45
sik2 <bickwang123@gmail.com>
2026-03-17 12:12:19 +0900

    23

--- src/components/TodoWriteForm.jsx
+++ src/components/TodoWriteForm.jsx
@@ -1,11 +1,21 @@
-import { useTodos } from '../context/TodoContext'
-
 function TodoWriteForm() {
-    const { addTodo } = useTodos()
+    // const { addTodo } = useTodos()
+
     const hadleOnsubmit = (e) => {
         e.preventDefault()
         const form = e.target
-        addTodo(form.todo.value)
+
+        fetch('https://dummyjson.com/todos/add', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                todo: form.todo.value,
+                completed: false,
+                userId: 5,
+            }),
+        })
+
+        // addTodo(form.todo.value)
     }
 
     return (
